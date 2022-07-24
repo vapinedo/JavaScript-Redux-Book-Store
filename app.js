0a63937d47accd7c04146cc6b076871e68017877
Victor Pinedo <victorpinedomartinez@gmail.com>
2022-07-24 11:04:35 -0500

    [add] detail button

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const getBookList = async () => {
-  const url = `https://www.googleapis.com/books/v1/volumes?maxResults=5&orderBy=relevance&q=oliver%20sacks`;
+  const url = `https://www.googleapis.com/books/v1/volumes?maxResults=10&orderBy=relevance&q=oliver%20sacks`;
   const response = await fetch(url);
   const data = await response.json();
-  return data.items;
+  const bookList = data.items;
+  console.log(bookList);
+  return bookList;
 };
 
 const getXCharFromText = (text, countChar) => {
@@ -15,40 +17,52 @@ const getXCharFromText = (text, countChar) => {
 };
 
 const createCard = (cardData, targetDOM) => {
-  const title = cardData.volumeInfo.title;
-  const img = cardData.volumeInfo.imageLinks.thumbnail;
-  const description = getXCharFromText(cardData.volumeInfo.description, 140);
+  const cardTitle = cardData.volumeInfo.title;
+  const seeDetail = cardData.volumeInfo.infoLink;
+  const cardImg = cardData.volumeInfo.imageLinks.thumbnail;
+  const cardDescription = getXCharFromText(cardData.volumeInfo.description, 140);
 
-  console.log({ cardData });
+  const container = document.createElement("div");
+  container.className = "col";
 
-  const cardWrapperEl = document.createElement("div");
-  cardWrapperEl.className = "col";
+  const card = document.createElement("div");
+  card.className = "card";
 
-  const cardInnerWrapperEl = document.createElement("div");
-  cardInnerWrapperEl.className = "card";
+  const img = document.createElement('img');
+  img.className = 'card-img-top';
+  img.setAttribute('src', cardImg);
 
-  const cardImg = document.createElement('img');
-  cardImg.className = 'card-img-top';
-  cardImg.setAttribute('src', img);
+  const body = document.createElement("div");
+  body.className = "card-body";
 
-  const bodyEl = document.createElement("div");
-  bodyEl.className = "card-body";
 
-  const bodyTitleEl = document.createElement("h5");
-  bodyTitleEl.className = "card-title";
-  bodyTitleEl.innerText = title;
+  const linkWrapper = document.createElement('div');
+  linkWrapper.className = 'd-grid mt-3';
 
-  const bodyTextEl = document.createElement("p");
-  bodyTextEl.className = "card-text";
-  bodyTextEl.innerText = description;
-  
+  const detailLink = document.createElement('a');
+  detailLink.className = 'btn btn-outline-primary';
+  detailLink.setAttribute('href', seeDetail);
+  detailLink.setAttribute('target', '_blank');
+  detailLink.innerText = 'See Details';
 
-  bodyEl.appendChild(bodyTitleEl);
-  bodyEl.appendChild(bodyTextEl);
-  cardInnerWrapperEl.appendChild(cardImg);
-  cardInnerWrapperEl.appendChild(bodyEl);
-  cardWrapperEl.appendChild(cardInnerWrapperEl);
-  targetDOM.appendChild(cardWrapperEl);
+  const title = document.createElement("h5");
+  title.className = "card-title";
+  title.innerText = cardTitle;
+
+  const text = document.createElement("p");
+  text.className = "card-text";
+  text.innerText = cardDescription;  
+
+  body.appendChild(title);
+  body.appendChild(text);
+  linkWrapper.appendChild(detailLink);
+  body.appendChild(linkWrapper);
+
+  card.appendChild(img);
+  card.appendChild(body);
+
+  container.appendChild(card);
+  targetDOM.appendChild(container);
 };
 
 const main = async () => {
